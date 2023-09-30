65231cbe71e8b2513faa7bb5c54b0c776481cf4d
minheeyoon <mini940511@gmail.com>
2023-10-01 06:52:42 +0900

    add react project

---
+++ src/components/ProjectList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// 아래글이 답일 수 있음
+//https://www.npmjs.com/package/markdown-to-jsx 리액트 마크다운 공식문서 
+//https://github.com/likesan/3rd-blog-netlify/blob/master/src/posts/2019-09-30-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EB%B8%94%EB%A1%9C%EA%B7%B8%EC%97%90-%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4-%ED%8F%AC%EC%8A%A4%ED%8A%B8%EB%A5%BC-%EB%82%98%ED%83%80%EB%82%B4%EB%B3%B4%EC%9E%90.md
+
+const ProjectList = () => {
+    return (
+
+        <div className="container">
+            <h1> 테스트 </h1>
+        </div>
+
+    )
+}
+
+export default ProjectList
---
+++ src/pages/ProjectPost.js
@@ -0,0 +1,32 @@
+import Markdown from "react-markdown";
+import { useEffect, useState } from "react";
+
+
+
+
+
+const ProjectPost = () => {
+
+    const [postContents, setPostContent] = useState("");
+
+
+    useEffect(() => {
+        import("../projects/project02.md")
+            .then(res => {
+                fetch(res.default)
+                    .then(response => response.text())
+                    .then(response => setPostContent(response))
+                    .catch(err => console.log(err))
+            })
+    }, [])
+
+    return (
+        <article>
+            <Markdown>
+                {postContents}
+            </Markdown>
+        </article>
+    )
+}
+
+export default ProjectPost
